c4f6757331541bd93ce1f45ee61ace7762b7f791
AmirHosein <amirhsh8510@gmail.com>
2026-05-31 20:46:07 +0330

    overwritten codes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+//class based component
+import {Component, createContext} from 'react' //"Component" is a calss inside react module
+// class App extends Component{
+//     render(){
+//         return ( // this syntax  "<></>" is called react fragment
+//             <>
+//             <h1>hello</h1>
+//             <h2>hello</h2>
+//             </>
+//         )
+        
+//     }//it returns a reactElement (jsx) and cannot returns two tags of jsx
+// }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(){
+    return  ( // this syntax  "<></>" is called react fragment
+            <>
+            <h1>hello</h1>
+            <h2>hello</h2>
+            </>
+        )
 }
 
-export default App;
+export default App
+
+//there are keyshort for easier coding such as 'imrc'(it imports the React and component from react module),"cc"(it creates a class based component),"sfc"(it creates a functional component)
+
--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react"
+import ReactDOM from "react-dom/client"
+import App from'./App'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Product from "./components/product"
+// const reactElement=<h1>hello world</h1>
+// ReactDOM.render(reactElement,document.getElementById('root'))
+const root=ReactDOM.createRoot(document.getElementById('root'))
+// root.render(reactElement)//the first argument is a jsx
+// const root1=ReactDOM.createRoot(document.getElementById('root1'))
+root.render([<App/>,<Product/>])//for Component the programmer must use this syntax
+// root1.render(<Product/>)
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//components with a state are called stateful and those without state are called stateless
